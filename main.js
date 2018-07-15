3df5d030a5ab3899e1c4de64a2d9fbfb61c59739
Victor <victor.ananias8@gmail.com>
2018-07-14 21:54:08 -0300

    fim

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
-Vue.component('tarefa', {
+Vue.component('lista-tarefas', {
+  template:
   /*
   |
-  | dentro do slot é possível recever texto do parent
+  | deve sempre existir um componente pai em componentes Vue
   |
   */
-  template: `<li><slot></slot></li>`,
-  /*
-  |
-  | dentro de um component, 'data' deve ser uma função que retorna um objeto
-  |
-  */
+  `<ul>
+      <tarefa v-for="tarefa in tarefas" v-text="tarefa.texto"></tarefa>
+    </ul>`,
   data() {
-    return {};
+    return {
+      tarefas: [
+        { texto: "Aprender Vue.js", completa: false },
+        { texto: "Aprender Laravel", completa: true },
+        { texto: "Aprender AngularJS", completa: true },
+        { texto: "Aprender JavaScript", completa: true },
+        { texto: "Aprender Angular", completa: false },
+        { texto: "Aprender HTML", completa: true },
+        { texto: "Aprender React", completa: false },
+        { texto: "Aprender NoSQL", completa: false },
+        { texto: "Se tornar Super Full Stack", completa: false }
+      ]
+    }
   }
 });
 
+Vue.component('tarefa', {
+  template: `<li><slot></slot></li>`,
+});
+
 new Vue({
-  el: '#root',
-  data: {
-    tarefas: [
-      { texto: "Aprender Vue.js", completa: false },
-      { texto: "Aprender Laravel", completa: true },
-      { texto: "Aprender AngularJS", completa: true },
-      { texto: "Aprender JavaScript", completa: true },
-      { texto: "Aprender Angular", completa: false },
-      { texto: "Aprender HTML", completa: true },
-      { texto: "Aprender React", completa: false },
-      { texto: "Aprender NoSQL", completa: false },
-      { texto: "Se tornar Super Full Stack", completa: false }
-    ]
-  }
+  el: '#root'
 });
