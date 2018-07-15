dcfa48d6570e5f4e9fc6038b4bd6c6f3fb84b6da
Victor <victor.ananias8@gmail.com>
2018-07-15 13:47:21 -0300

    fim

--- main.js
+++ main.js
@@ -5,13 +5,42 @@ Vue.component('modal', {
   |
   */
   template: `
-  <div class="modal is-active">
-    <div class="modal-background"></div>
-    <div class="modal-content">
-      <div class="box"><slot></slot></div>
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">`
+          /*
+          |
+          | É possível dar nomes aos slots
+          |
+          */
+          +`<slot name="titulo"></slot>
+          </p>
+          <button class="delete" aria-label="close" @click="fechar"></button>
+        </header>
+        <section class="modal-card-body">
+          <slot>`
+          /*
+          |
+          | Dentro dos slots é possível definir conteúdo default
+          |
+          */
+          +`Conteúdo Default
+          </slot>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success" @click="fechar">Salvar</button>
+          <button class="button" @click="fechar">Cancelar</button>
+        </footer>
+      </div>
     </div>
-    <button class="modal-close is-large" aria-label="close" @click="$emit('fechar')"></button>
-  </div>`
+  `,
+  methods: {
+    fechar() {
+      this.$emit('fechar');
+    }
+  }
 });
 
 new Vue({
