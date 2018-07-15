88e266b37d6d1f45c89f49e4324b8ad7b64b4363
Victor <victor.ananias8@gmail.com>
2018-07-15 13:56:12 -0300

    fim

--- main.js
+++ main.js
@@ -1,51 +1,22 @@
-Vue.component('modal', {
-  /*
-  |
-  | $emit('eventoPersonalizado') pode ser usado para emitir um evento personalizado
-  |
-  */
-  template: `
-    <div class="modal is-active">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">`
-          /*
-          |
-          | É possível dar nomes aos slots
-          |
-          */
-          +`<slot name="titulo"></slot>
-          </p>
-          <button class="delete" aria-label="close" @click="fechar"></button>
-        </header>
-        <section class="modal-card-body">
-          <slot>`
-          /*
-          |
-          | Dentro dos slots é possível definir conteúdo default
-          |
-          */
-          +`Conteúdo Default
-          </slot>
-        </section>
-        <footer class="modal-card-foot">
-          <button class="button is-success" @click="fechar">Salvar</button>
-          <button class="button" @click="fechar">Cancelar</button>
-        </footer>
-      </div>
-    </div>
-  `,
+/*
+|
+| É possível utilizar components para somente atribuir comportamentos
+| a pedaços de HTML
+|
+*/
+Vue.component('progresso-view', {
+  data() {
+    return {
+      porcentagem: 40
+    }
+  },
   methods: {
-    fechar() {
-      this.$emit('fechar');
+    aumentar() {
+      this.porcentagem += 10;
     }
   }
 });
 
 new Vue({
-  el: '#root',
-  data: {
-    exibir: false
-  }
+  el: '#root'
 });
