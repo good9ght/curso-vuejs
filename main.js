4af136c219e0f477b4398e4216ec488875b7ebc7
Victor <victor.ananias8@gmail.com>
2018-07-14 22:15:06 -0300

    fim

--- main.js
+++ main.js
@@ -1,32 +1,20 @@
-Vue.component('lista-tarefas', {
-  template:
-  /*
-  |
-  | deve sempre existir um componente pai em componentes Vue
-  |
-  */
-  `<ul>
-      <tarefa v-for="tarefa in tarefas" v-text="tarefa.texto"></tarefa>
-    </ul>`,
+Vue.component('mensagem', {
+  props: [ 'titulo', 'texto' ],
+  template: `
+    <article class="message" v-show="exibir">
+      <div class="message-header">
+        <p>{{ titulo }}</p>
+        <button class="delete" aria-label="delete" @click="exibir = false"></button>
+      </div>
+      <div class="message-body">
+        {{ texto }}
+      </div>
+    </article>`,
   data() {
     return {
-      tarefas: [
-        { texto: "Aprender Vue.js", completa: false },
-        { texto: "Aprender Laravel", completa: true },
-        { texto: "Aprender AngularJS", completa: true },
-        { texto: "Aprender JavaScript", completa: true },
-        { texto: "Aprender Angular", completa: false },
-        { texto: "Aprender HTML", completa: true },
-        { texto: "Aprender React", completa: false },
-        { texto: "Aprender NoSQL", completa: false },
-        { texto: "Se tornar Super Full Stack", completa: false }
-      ]
+      exibir: true
     }
   }
-});
-
-Vue.component('tarefa', {
-  template: `<li><slot></slot></li>`,
 });
 
 new Vue({
