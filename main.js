29336a7170fd08080819203b9384aa3e32f81e9e
Victor <victor.ananias8@gmail.com>
2018-07-15 11:51:53 -0300

    save

--- main.js
+++ main.js
@@ -1,22 +1,65 @@
-Vue.component('modal', {
-  /*
-  |
-  | $emit('eventoPersonalizado') pode ser usado para emitir um evento personalizado
-  |
-  */
+Vue.component('tabs', {
+  // é necessario definir o slot para que hajam $children elements
   template: `
-  <div class="modal is-active">
-    <div class="modal-background"></div>
-    <div class="modal-content">
-      <div class="box"><slot></slot></div>
+    <div>
+      <div class="tabs">
+        <ul>
+          <li :class="{ 'is-active': tab.ativa }" v-for="tab in tabs">
+            <a @click="selecionar(tab)" :href="tab.href">{{ tab.nome }}</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <slot></slot>
+      </div>
     </div>
-    <button class="modal-close is-large" aria-label="close" @click="$emit('fechar')"></button>
-  </div>`
+  `,
+  methods: {
+    selecionar(tabSelecionada) {
+      this.tabs.forEach(tab => {
+        tab.ativa = (tab.nome == tabSelecionada.nome);
+      });
+    }
+  },
+  data() {
+    return { tabs: [] }
+  },
+  created() {
+    this.tabs = this.$children;
+  }
+});
+
+Vue.component('tab', {
+  props: {
+    /*
+    |
+    | É possivel definir props do componente com valores default ou required
+    |
+    */
+    nome: { required: true },
+    selecionada: { default: false }
+  },
+  template: `
+    <div v-show="ativa"><slot></slot></div>
+  `,
+  data() {
+    return {
+      /*
+      |
+      | Vue não permite que props sejam alteradas diretamente,
+      | ativa deve ser uma variável dinâmina, logo, não pode ser uma prop
+      |
+      */
+      ativa: this.selecionada
+    }
+  },
+  computed: {
+    href() {
+      return "#" + this.nome.toLowerCase().replace(/ /g, "-");
+    }
+  }
 });
 
 new Vue({
-  el: '#root',
-  data: {
-    exibir: false
-  }
+  el: '#root'
 });
