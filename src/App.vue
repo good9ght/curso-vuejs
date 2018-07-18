ae3b36f23c431972c20e316ac1c35f4fcefb7a06
Victor <victor.ananias8@gmail.com>
2018-07-17 21:02:00 -0300

    hello world

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <mensagem>{{ msg }}</mensagem>
+  </div>
+</template>
+
+<script>
+import Mensagem from "./components/Mensagem.vue";
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello World'
+    }
+  },
+  components: {
+    Mensagem
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Mensagem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mensagem">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default { }
+</script>
+
+<style>
+    .mensagem {
+        background-color: #e3e3e3;
+        padding: 1em;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        border-radius: 0.2em;
+        width: 7vw;
+        margin: auto;
+        margin-top: 30vh;
+    }
+</style>
